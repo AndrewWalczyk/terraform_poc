b68319389e48eb04b865b4bcfe41e051fdc26c71
AndrewWalczyk <drewwalczyk@gmail.com>
2024-01-24 14:35:05 -0600

    fixing main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ module "vcn" {
 
   source = "github.com/oracle-terraform-modules/terraform-oci-vcn"
   # to use the terraform registry version comment the previous line and uncomment the 2 lines below
-  # source  = "oracle-terraform-modules/vcn/oci"
-  # version = "specify_version_number"
+   source  = "oracle-terraform-modules/vcn/oci"
+   version = "specify_version_number"
 
   # general oci parameters
   compartment_id = var.compartment_id
